e65773ae894c35adca6567529aada937ad373799
Anton Bombin <step4321@mail.ru>
2024-06-18 19:04:22 +0300

    added hometask

---
+++ hometask3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void Reverse(int[] array, int i)
+{
+    if (i < 0) return;
+    Console.Write(array[i] + " ");
+    Reverse(array, i - 1);
+}
+
+int[] array = { 7, 18, 3, 94, 32 };
+Reverse(array, array.Length - 1);
